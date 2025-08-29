33c9b629976edf3ac05da783a9dba6bef9a90212
Mahedi0x <mahedih.cse@gmail.com>
2025-08-29 18:45:38 +0600

    copy count functionality done

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+//=============================   Copy Button Functionality ================
 document.addEventListener("DOMContentLoaded", () => {
-    const favCount = document.getElementById("heart-count");
-    const heartIcons = document.querySelectorAll(".fa-heart");
-  
-    heartIcons.forEach(icon => {
-      icon.addEventListener("click", () => {
-        let current = parseInt(favCount.textContent, 10);
-        favCount.textContent = current + 1;
-      });
+  const heartCount = document.getElementById("heart-count");
+  const heartIcons = document.querySelectorAll(".fa-heart");
+
+  heartIcons.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      let current = parseInt(heartCount.textContent, 10);
+      heartCount.textContent = current + 1;
     });
   });
+});
+
+
+//=============================   Copy Button Functionality ================
+document.querySelectorAll(".copy-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const copyCount = document.getElementById("copy-count");
+    let current = parseInt(copyCount.textContent, 10);
+    copyCount.textContent = current + 1;
+    const cardContainer = btn.closest(".card");
+    const serviceNumber = cardContainer.querySelector(".text-3xl").innerText;
+    navigator.clipboard.writeText(serviceNumber).then(() => {
+      alert(`নাম্বার কপি হয়েছে: ${serviceNumber}`);
+    });
+  });
+});
+
